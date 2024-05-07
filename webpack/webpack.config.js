9fbb13ea2253c8382c765764ee832634853ab91c
Andreas Neumann <andreas.neumann@sz.de>
2024-05-07 10:46:32 +0200

    naming and icons

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,7 +3,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
    mode: "production",
    entry: {
-      background: path.resolve(__dirname, "..", "src", "piano_helper.js"),
+      piano_helper: path.resolve(__dirname, "..", "src", "piano_helper.js"),
    },
    output: {
       path: path.join(__dirname, "../dist"),
